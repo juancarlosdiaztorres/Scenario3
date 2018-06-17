5c18518c0d8500978d42ec4eb4a1901117c33220
juancarlosdiaztorres <jcdiaztorres96@gmail.com>
2018-06-18 00:13:31 +0200

    change fields, more errors appear

--- conf/ONOS/start-ONOS.sh
+++ conf/ONOS/start-ONOS.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-export ONOS_APPS="drivers,openflow,segmentrouting"
+export ONOS_APPS="drivers,openflow-base,lldpprovider,netcfghostprovider,segmentrouting"
 service onos start
 sleep 30
 /opt/onos/karaf/bin/client "app activate org.onosproject.drivers"
 sleep 30
-/opt/onos/karaf/bin/client "app activate org.onosproject.openflow"
+/opt/onos/karaf/bin/client "app activate org.onosproject.openflow-base"
+sleep 30
+/opt/onos/karaf/bin/client "app activate org.onosproject.lldpprovider"
+sleep 30
+/opt/onos/karaf/bin/client "app activate org.onosproject.netcfghostprovider"
 sleep 30
 /opt/onos/karaf/bin/client "app activate org.onosproject.segmentrouting"
 
